d526edf014b39987df32efbec514a3c7407d02c6
Jerry Zhao <jerryz123@berkeley.edu>
2024-03-13 13:57:30 -0700

    Relax fence restriction on amos

--- src/main/scala/exu/Core.scala
+++ src/main/scala/exu/Core.scala
@@ -377,7 +377,7 @@ class ShuttleCore(tile: ShuttleTile, edge: TLEdgeOut)(implicit p: Parameters) ex
     val uop = rrd_uops(i).bits
     val ctrl = uop.ctrl
     val vec_bsy = io.vector.map(v => v.backend_busy || v.trap_check_busy).getOrElse(false.B)
-    val rrd_fence_stall = (i == 0).B && ((uop.system_insn || ctrl.fence || uop.sfence || ctrl.amo || ctrl.fence_i) &&
+    val rrd_fence_stall = (i == 0).B && ((uop.system_insn || ctrl.fence || uop.sfence || ctrl.fence_i) &&
       (ex_bsy || mem_bsy || wb_bsy || isboard_bsy || fsboard_bsy || !io.dmem.ordered || io.rocc.busy || vec_bsy))
     val rrd_rocc_stall = (i == 0).B && ctrl.rocc && !io.rocc.cmd.ready
     val is_pipe0 = (uop.system_insn
